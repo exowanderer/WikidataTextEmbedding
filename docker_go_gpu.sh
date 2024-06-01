47eabe149149010a8bdedfc7b018845da52b1190
Jonathan Fraine <jdfraine@gmail.com>
2024-06-01 16:00:51 +0200

    added seperate go command for docker with gpus

---
+++ docker_go_gpu.sh
@@ -0,0 +1,15 @@
+# Build the Docker image
+docker build -t wikidata_datadump_textification .
+
+# Run the Docker container with the environment variable and volume mounting
+docker run -it \
+        -v $HOME/Research/Wikidata:/app/datadump \
+        -v $(pwd)/csvfiles:/app/csvfiles \
+        -v $(pwd)/sqlitedbs:/app/sqlitedbs \
+        -v $HOME/.cache/huggingface/hub:/root/.cache/huggingface/hub \
+        -e WIKIMEDIA_TOKEN=$WIKIMEDIA_TOKEN \
+        -e N_COMPLETE=100 \
+        -e EMBED=True \
+        -e EMBED_BATCHSIZE=100 \
+        --gpus all \
+        wikidata_datadump_textification
