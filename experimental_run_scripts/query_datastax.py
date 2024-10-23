057db784a31b05e31d0cf400752e4ac414ce8b36
exowanderer <jdfraine@gmail.com>
2024-10-24 00:23:58 +0200

    Included return operation query datastax script

--- experimental_run_scripts/query_datastax.py
+++ experimental_run_scripts/query_datastax.py
@@ -3,20 +3,10 @@ import argparse
 from langchain_astradb import AstraDBVectorStore
 from astrapy.info import CollectionVectorServiceOptions
 
-def main():
-    # Set up argument parsing
-    parser = argparse.ArgumentParser(description="Search using AstraDBVectorStore and NVIDIA embeddings")
-
-    # Arguments
-    parser.add_argument('--api_key_json', required=True, help='Path to the API key JSON file')
-    parser.add_argument('--query', '-q', required=True, help='User query for similarity search')
-    parser.add_argument('--k', '-k', type=int, default=10, help='Number of top results to return')
-
-    # Parse arguments
-    args = parser.parse_args()
+def query_similarity(query, api_key_json, k=10):
 
     # Load API key from JSON file
-    datastax_token = json.load(open(args.api_key_json))
+    datastax_token = json.load(open(api_key_json))
     ASTRA_DB_DATABASE_ID = datastax_token['ASTRA_DB_DATABASE_ID']
     ASTRA_DB_APPLICATION_TOKEN = datastax_token['ASTRA_DB_APPLICATION_TOKEN']
     ASTRA_DB_API_ENDPOINT = datastax_token["ASTRA_DB_API_ENDPOINT"]
@@ -38,12 +28,25 @@ def main():
     )
 
     # Perform similarity search using the user-provided query and k value
-    results = graph_store.similarity_search(args.query, k=args.k)
+    results = graph_store.similarity_search(query, k=k)
 
     # Print the results
     for result in results:
-        print(result.metadata['QID'])
-        print(result.page_content)
+        print()
+        print(f"{result.metadata['QID']}:\n{result.page_content}")
+
+    return results
 
 if __name__ == "__main__":
-    main()
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description="Search using AstraDBVectorStore and NVIDIA embeddings")
+
+    # Arguments
+    parser.add_argument('--api_key_json', required=True, help='Path to the API key JSON file')
+    parser.add_argument('--query', required=True, help='User query for similarity search')
+    parser.add_argument('--k', type=int, default=10, help='Number of top results to return')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    results = query_similarity(args.query, args.api_key_json, args.k)
